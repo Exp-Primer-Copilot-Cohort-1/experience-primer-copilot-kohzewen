ce92f9af931936e482aefa873a894cf57e44026d
kohzewen <56101543+kohzewen@users.noreply.github.com>
2023-09-20 17:56:37 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// A function that creates a web server
+
+
+
+// Import the express module
+const express = require('express');
+// Create an express application
+const app = express();
+// Import the body-parser module
+const bodyParser = require('body-parser');
+// Import the path module
+const path = require('path');
